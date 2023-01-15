923c82cd239c860b98e0685479a7070d23c8e7c7
bigDre82 <andre.oscco.14@gmail.com>
2023-01-15 20:38:30 +0100

    Validaciones

--- FRONT -ANGULAR-authApp/src/app/auth/services/auth.service.ts
+++ FRONT -ANGULAR-authApp/src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { catchError, map, tap } from 'rxjs/operators'
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environments';
 import { AuthResponse, Usuario } from '../interfaces/interfaces';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -37,7 +37,7 @@ export class AuthService {
           uid: resp.uid!
         }
       }
-      console.log(resp);
+
     } ),
     map(resp => resp.ok), catchError(err => of(err.error.msg) )
    )
@@ -45,11 +45,25 @@ export class AuthService {
 
 // ================================================================================
 
-  validarToken() {
+  validarToken(): Observable<boolean>{
     const url = `${ this.baseUrl}/auth/renew`;
     const headers = new HttpHeaders().set('x-token', localStorage.getItem('token') || '');
 
-  return this.http.get(url, { headers});
+  return this.http.get<AuthResponse>(url, { headers}).pipe( map( resp => {
+
+    console.log(resp.token);
+    
+    localStorage.setItem('token',resp.token!);
+
+        this._usuario = {
+          name: resp.name!,
+          uid: resp.uid!
+        }
+
+    return resp.ok;
+  }),
+  catchError(err => of(false))
+  )
 
 
   }
